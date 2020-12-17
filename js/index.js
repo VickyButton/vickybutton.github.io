b05f2d45b2bff5c07321f2ffec2b0db62696d0af
Vicky <vickythewebdev@gmail.com>
2020-12-17 04:28:19 -0500

    Added content to About section and added a Scroll-to-top button

--- js/index.js
+++ js/index.js
@@ -13,15 +13,20 @@ function toggleMenu() {
     hamburgerMenu.classList.toggle('pushed');
 }
 
+window.addEventListener('resize', () => {
+    if(hamburgerMenu.classList.contains('pushed'))
+        toggleMenu();
+})
+
 document.addEventListener('scroll', () => {
     if(hamburgerMenu.classList.contains('pushed'))
         toggleMenu();
 })
 
-// document.body.addEventListener('touchstart', e => {
-//     if(hamburgerMenu.classList.contains('pushed') && e.target.id != 'pullout-menu' && e.target.id != 'hamburger-button' && e.target.parentNode.id != 'pullout-menu')
-//         toggleMenu();
-// });
+document.body.addEventListener('touchstart', e => {
+    if(hamburgerMenu.classList.contains('pushed') && e.target.id != 'pullout-menu' && e.target.id != 'hamburger-button' && e.target.parentNode.id != 'pullout-menu')
+        toggleMenu();
+});
 
 document.body.addEventListener('click', e => {
     if(hamburgerMenu.classList.contains('pushed') && e.target.id != 'pullout-menu' && e.target.id != 'hamburger-button' && e.target.parentNode.id != 'pullout-menu')
@@ -30,6 +35,12 @@ document.body.addEventListener('click', e => {
 
 document.getElementById('exit-button').addEventListener('click', toggleMenu);
 document.getElementById('hamburger-button').addEventListener('click', toggleMenu);
+document.getElementById('scroll-top-button').addEventListener('click', () => {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
 document.querySelectorAll('.menu-link').forEach(element => {
     element.addEventListener('click', toggleMenu());
 });
